e27f17307e2b7d256d4a19b58e51c737f56b3bd7
Sachin <sachin.rohit@live.com>
2015-09-27 15:54:06 +0530

    Transactions are sent to Remote MYSQL DB. This DB will be used in Android App and webserver to display reports

--- transactionsmanager.php
+++ transactionsmanager.php
@@ -69,7 +69,81 @@ function saveTransactionToDb() {
 			"totalValue" => $totalValue,
 			"Date" => $date 
 	) );
+	
+	saveTransactionToCloud();
+	
 	print ('{}') ;
 }
-
+function connectToDB() {
+	$hostname = 'localhost';
+	$dsn = 'endurance';
+	$username = "root";//'samurai48';
+	$password = '';//'sachin48';
+	
+	try {
+		// $dbh = new PDO($dsn, $user, $password);
+		$dbh = new PDO ( "mysql:host=$hostname;dbname=endurancepos", $username, $password );
+		/*
+		$sql = "SELECT * FROM endurance";
+		foreach ( $dbh->query ( $sql ) as $row ) {
+			echo $row ["collection_brand"] . " - " . $row ["collection_year"] . "<br/>";
+		}*/
+		return $dbh;
+	} catch ( PDOException $e ) {
+		echo 'Connection failed: ' . $e->getMessage ();
+	}
+}
+function saveTransactionToCloud() {
+	// TODO
+	// Very similar to saveTransactionsToDb()/
+	// But put data in Mysql DB.
+	// Table => Store Specific
+	/*
+	 * Data to be saved = > "transactionID" => $traxn_id, "ItemsQty" => "", "OrderType" => "", "totalValue" => $totalValue, "Date" => $date
+	 */
+	
+	print(' saveTransactionToCloud ->');
+	
+	$ordertype =  $_POST ['ordertype'];
+	$totalValue =  $_POST ['totalVal'];
+	$date = $_POST ['date'];
+	$traxn_id = $_POST ['transactionid'];
+	$transactionItems = serialize ( json_decode ( $_POST ['json'] ) );
+	
+	//$transactionItems = "items";
+	
+	$dbh  =  connectToDB();
+	$dbh->setAttribute ( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	
+	$ret = true;
+	$device_specific_table = "rest_dev123";
+	
+	try {
+	$statement = $dbh->prepare ( "INSERT INTO $device_specific_table (ordertype, totalvalue , orderdate , ordertime , itemdetails)
+    VALUES(:ordertype, :totalvalue, :orderdate , :ordertime , :itemdetails )" );
+	$ret = $statement->execute ( array (
+			"ordertype" => $ordertype,
+			"totalvalue" => $totalValue,
+			"orderdate" => $date,
+			"ordertime" => $date,
+			"itemdetails" => $transactionItems,
+			
+	) );
+	
+	}	catch (PDOException $e) {
+		echo $e->getMessage();//Remove or change message in production code
+		print ($e->getMessage());
+	}
+	
+	if($ret) {
+		print('OK Execution');
+	}else {
+		print ('KO ');
+	}
+	print(' saveTransactionToCloud <-');
+	/*
+	"ordertime" => $date,
+	"itemdetails" => $transactionItems,
+	*/
+}
 ?>
